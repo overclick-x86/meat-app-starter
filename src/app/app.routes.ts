24e364277ddae5d76d9e0e7712e37a2b41658904
Marcio Anderson Cezar de Araujo <contato@overclick.com.br>
2019-12-18 14:35:24 -0300

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,22 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
+import { RestaurantsComponent } from './restaurants/restaurants.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
 
 export const ROUTES: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'restaurants', component: RestaurantsComponent },
+  {
+    path: 'restaurants/:id', component: RestaurantDetailComponent,
+    children: [
+      { path: '', redirectTo: 'menu', pathMatch: 'full' },
+      { path: 'menu', component: MenuComponent },
+      { path: 'reviwes', component: ReviewsComponent }
+    ]
+  },
   { path: 'about', component: AboutComponent }
 
 ]
