c383d2d880ec2d3ea0ff71f0741ac9cc375845e3
Marcio Anderson Cezar de Araujo <contato@overclick.com.br>
2020-01-16 19:24:27 -0300

    update

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { MenuComponent } from './restaurant-detail/menu/menu.component';
 import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
-import { OrderComponent } from './order/order.component';
 import { OrderSumaryComponent } from './order-sumary/order-sumary.component';
 
 export const ROUTES: Routes = [
@@ -19,8 +17,8 @@ export const ROUTES: Routes = [
       { path: 'reviwes', component: ReviewsComponent }
     ]
   },
-  { path: 'about', component: AboutComponent },
-  { path: 'order', component: OrderComponent },
-  { path: 'order-sumary', component: OrderSumaryComponent }
+  { path: 'order', loadChildren: './order/order.module#OrderModule' },
+  { path: 'order-sumary', component: OrderSumaryComponent },
+  { path: 'about', loadChildren: './about/about.module#AboutModule' }
 
 ]
--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms'
+
 import { RadioOption } from 'app/shared/radio/radio-option.model';
 import { OrderService } from './order.service';
 import { CartItem } from 'app/restaurant-detail/shopping-cart/cart-item.model';
@@ -11,6 +13,13 @@ import { Router } from '@angular/router';
 })
 export class OrderComponent implements OnInit {
 
+  emailPattern = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+
+  numberPattern = /^[0-9]*$/
+
+  // Formulário
+  orderForm: FormGroup
+
   delivery: number = 8
 
   paymentOptions: RadioOption[] = [
@@ -21,10 +30,34 @@ export class OrderComponent implements OnInit {
 
   constructor(
     private orderService: OrderService,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder
   ) { }
 
   ngOnInit() {
+    this.orderForm = this.formBuilder.group({
+      name: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+      email: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      emailConfirmation: this.formBuilder.control('', [Validators.required, Validators.pattern(this.emailPattern)]),
+      address: this.formBuilder.control('', [Validators.required, Validators.minLength(5)]),
+      number: this.formBuilder.control('', [Validators.required, Validators.pattern(this.numberPattern)]),
+      optionalAddress: this.formBuilder.control(''),
+      paymentOption: this.formBuilder.control('', [Validators.required]),
+    }, { validator: OrderComponent.equalsTo })
+  }
+
+  static equalsTo(group: AbstractControl): { [key: string]: boolean } {
+    const email = group.get('email')
+    const emailConfirmation = group.get('emailConfirmation')
+    if (!email || !emailConfirmation) {
+      return undefined
+    }
+
+    if (email.value !== emailConfirmation.value) {
+      return { emailsNotMatch: true }
+    }
+
+    return undefined
   }
 
   itensValue(): number {
